1ef5be28eb6519d7291d71485af8248d4ca78516
Zian Kang <114457597+YetStillAGod@users.noreply.github.com>
2024-11-18 01:53:47 -0500

    HW_1116&Modification on project

--- Project/data/data.py
+++ Project/data/data.py
@@ -18,6 +18,7 @@ n=pd.qcut(m,5)
 df=df.groupby([n,'Body Style']).size()
 df=df.unstack()
 df.plot(kind='bar')
+c_1=round(df.corr(),2)
 
 df = pd.read_csv(r"E:\UR\Math\203\Project\data\1.csv")
 
@@ -25,5 +26,16 @@ df=df.groupby(["Dealer_Region",'Body Style']).size()
 df=df.unstack()
 df.plot(kind='bar')
 df.plot()
+c_2=round(df.corr(),2)
+
+print(c_1)
+print(c_2)
+
+df = pd.DataFrame({"Dealer Region":["Aurora","Austin","Greenville","Janesville","Middletown","Pasco","Scottsdale"],"Frequency":[3130,4135,3128,3821,3128,3131,3433]})
+df.plot(x="Dealer Region",y="Frequency",kind="bar")
+
+df = pd.DataFrame({"Car Model":["Passenger","Sedan","Hatchback","SUV","Hardtop"],"Frequency":[3945,4488,6128,6374,2970]})
+df.plot(x="Car Model",y="Frequency",kind="bar")
+
 
 plt.show()
